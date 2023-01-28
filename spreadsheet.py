33ee83dcff0b4476df03fbfe71552d70d120a3e1
Doru <dorubrk@gmail.com>
2023-01-28 15:33:45 +0200

    housing CSV

---
+++ spreadsheet.py
@@ -0,0 +1,14 @@
+import csv
+
+f=open("housing.csv",encoding='latin-1')
+csvreader=csv.reader(f)
+
+header=[]
+header=next(csvreader)
+
+
+rows=[]
+for row in csvreader:
+    rows.append(row)
+
+f.close()
