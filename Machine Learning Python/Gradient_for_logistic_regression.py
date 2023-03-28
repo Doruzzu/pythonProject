eefa43c47807a034b60b80fb2eb5df6669648db1
Doru <dorubrk@gmail.com>
2023-03-28 10:42:24 +0300

    Gradient Descend for  Logistic regression

---
+++ Machine Learning Python/Gradient_for_logistic_regression.py
@@ -0,0 +1,178 @@
+import math
+import numpy as np
+
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+    Args:
+        z (ndarray): A scalar, numpy array of any size.
+    Returns:
+        g (ndarray): sigmoid(z), with the same shape as z
+    """
+    g = 1 / (1 + math.e ** (-1 * z))
+
+    return g
+
+
+def compute_cost(X, y, w, b, *argv):
+    """
+    Computes the cost over all examples
+    Args:
+      X : (ndarray Shape (m,n)) data, m examples by n features
+      y : (ndarray Shape (m,))  target value
+      w : (ndarray Shape (n,))  values of parameters of the model
+      b : (scalar)              value of bias parameter of the model
+      *argv : unused, for compatibility with regularized version below
+    Returns:
+      total_cost : (scalar) cost
+    """
+
+    m, n = X.shape
+    cost = 0
+    for i in range(m):
+        z = np.dot(X[i], w) + b
+        f_wb_i = sigmoid(z)
+        cost += -y[i] * np.log(f_wb_i) - (1 - y[i]) * np.log(1 - f_wb_i)
+
+    total_cost = cost/m
+
+    return total_cost
+
+
+def compute_gradient(X, y, w, b, *argv):
+    """
+    Computes the gradient for logistic regression
+
+    Args:
+      X : (ndarray Shape (m,n)) data, m examples by n features
+      y : (ndarray Shape (m,))  target value
+      w : (ndarray Shape (n,))  values of parameters of the model
+      b : (scalar)              value of bias parameter of the model
+      *argv : unused, for compatibility with regularized version below
+    Returns
+      dj_dw : (ndarray Shape (n,)) The gradient of the cost w.r.t. the parameters w.
+      dj_db : (scalar)             The gradient of the cost w.r.t. the parameter b.
+    """
+    m, n = X.shape
+    dj_dw = np.zeros(w.shape)
+    dj_db = 0.
+
+    for i in range(m):
+        z_wb = 0
+        z_wb_ij = 0
+        for j in range(n):
+            z_wb_ij += X[i, j] * w[j]
+        z_wb += z_wb_ij
+        z_wb += b
+        f_wb = sigmoid(z_wb)
+
+        dj_db_i = f_wb - y[i]
+        dj_db += dj_db_i
+
+        for j in range(n):
+            dj_dw_ij = (f_wb - y[i]) * X[i][j]
+            dj_dw[j] += dj_dw_ij
+
+    dj_dw = dj_dw / m
+    dj_db = dj_db / m
+
+    return dj_db, dj_dw
+
+
+def predict(X, w, b):
+    """
+    Predict whether the label is 0 or 1 using learned logistic
+    regression parameters w
+
+    Args:
+      X : (ndarray Shape (m,n)) data, m examples by n features
+      w : (ndarray Shape (n,))  values of parameters of the model
+      b : (scalar)              value of bias parameter of the model
+
+    Returns:
+      p : (ndarray (m,)) The predictions for X using a threshold at 0.5
+    """
+    # number of training examples
+    m, n = X.shape
+    p = np.zeros(m)
+
+    ### START CODE HERE ###
+    # Loop over each example
+    for i in range(m):
+        z_wb = 0
+        # Loop over each feature
+        for j in range(n):
+            # Add the corresponding term to z_wb
+            z_wb += X[i, j] * w[j]
+
+        # Add bias term
+        z_wb += b
+
+        # Calculate the prediction for this example
+        f_wb = sigmoid(z_wb)
+
+        # Apply the threshold
+        if f_wb >= 0.5:
+            p[i] = 1
+
+    return p
+
+
+def compute_cost_reg(X, y, w, b, lambda_=1):
+    """
+    Computes the cost over all examples
+    Args:
+      X : (ndarray Shape (m,n)) data, m examples by n features
+      y : (ndarray Shape (m,))  target value
+      w : (ndarray Shape (n,))  values of parameters of the model
+      b : (scalar)              value of bias parameter of the model
+      lambda_ : (scalar, float) Controls amount of regularization
+    Returns:
+      total_cost : (scalar)     cost
+    """
+
+    m, n = X.shape
+
+    # Calls the compute_cost function that you implemented above
+    cost_without_reg = compute_cost(X, y, w, b)
+
+    # You need to calculate this value
+    reg_cost = 0.
+
+    ### START CODE HERE ###
+    for j in range(n):
+        reg_cost_j = w[j] ** 2
+        reg_cost = reg_cost + reg_cost_j
+
+    reg_cost = (lambda_ / (2 * m)) * reg_cost
+    ### END CODE HERE ###
+
+    # Add the regularization cost to get the total cost
+    total_cost = cost_without_reg + reg_cost
+
+    return total_cost
+
+
+def compute_gradient_reg(X, y, w, b, lambda_=1):
+    """
+    Computes the gradient for logistic regression with regularization
+
+    Args:
+      X : (ndarray Shape (m,n)) data, m examples by n features
+      y : (ndarray Shape (m,))  target value
+      w : (ndarray Shape (n,))  values of parameters of the model
+      b : (scalar)              value of bias parameter of the model
+      lambda_ : (scalar,float)  regularization constant
+    Returns
+      dj_db : (scalar)             The gradient of the cost w.r.t. the parameter b.
+      dj_dw : (ndarray Shape (n,)) The gradient of the cost w.r.t. the parameters w.
+
+    """
+    m, n = X.shape
+
+    dj_db, dj_dw = compute_gradient(X, y, w, b)
+
+    for i in range(n):
+        dj_dw[i] = dj_dw[i] + (lambda_ / m) * w[i]
+
+    return dj_db, dj_dw
