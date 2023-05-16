cc1c4b4d1e5ecdc62026fae934cad90cd24e9e23
Doru Bogdan <dorubrk@gmail.com>
2023-05-16 15:13:25 +0300

    Neural Network Tensorflow

---
+++ Neural_networks_tensorflow.py
@@ -0,0 +1,19 @@
+def eval_cat_err(y, yhat):
+    """
+    Calculate the categorization error
+    Args:
+      y    : (ndarray  Shape (m,) or (m,1))  target value of each example
+      yhat : (ndarray  Shape (m,) or (m,1))  predicted value of each example
+    Returns:|
+      cerr: (scalar)
+    """
+    m = len(y)
+    incorrect = 0
+    for i in range(m):
+
+        if y[i] != yhat[i]:
+            incorrect += 1
+
+    cerr = incorrect / m
+
+    return (cerr)
