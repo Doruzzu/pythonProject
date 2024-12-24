b168f6c4771fbd60bf7baf40e48c91410fc8a72e
Doru Bogdan <dorubrk@gmail.com>
2024-12-24 19:04:04 +0200

    Neural Networks

---
+++ Machine Learning Python/Neural_Network.py
@@ -0,0 +1,60 @@
+def sigmoid(z):
+    res = 1 / (1 + np.e ** (-z))
+
+    return res
+
+
+def layer_sizes(X, Y):
+    """
+    Arguments:
+    X -- input dataset of shape (input size, number of examples)
+    Y -- labels of shape (output size, number of examples)
+
+    Returns:
+    n_x -- the size of the input layer
+    n_h -- the size of the hidden layer
+    n_y -- the size of the output layer
+    """
+
+    n_x = X.shape[0]
+
+    n_h = 2
+
+    n_y = Y.shape[0]
+
+    return (n_x, n_h, n_y)
+
+
+def initialize_parameters(n_x, n_h, n_y):
+    """
+    Argument:
+    n_x -- size of the input layer
+    n_h -- size of the hidden layer
+    n_y -- size of the output layer
+
+    Returns:
+    params -- python dictionary containing your parameters:
+                    W1 -- weight matrix of shape (n_h, n_x)
+                    b1 -- bias vector of shape (n_h, 1)
+                    W2 -- weight matrix of shape (n_y, n_h)
+                    b2 -- bias vector of shape (n_y, 1)
+    """
+
+    ### START CODE HERE ### (~ 4 lines of code)
+    W1 = np.random.rand(n_h, n_x) * 0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.rand(n_y, n_h) * 0.1
+    b2 = np.zeros((n_y, 1))
+    ### END CODE HERE ###
+
+    assert (W1.shape == (n_h, n_x))
+    assert (b1.shape == (n_h, 1))
+    assert (W2.shape == (n_y, n_h))
+    assert (b2.shape == (n_y, 1))
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+
+    return parameters
