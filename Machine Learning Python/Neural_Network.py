851f5233a2ed4da72e651181d41b83a4b12d76ef
Doru Bogdan <dorubrk@gmail.com>
2025-01-03 13:47:37 +0200

    Neural Networks Update Parameters

--- Machine Learning Python/Neural_Network.py
+++ Machine Learning Python/Neural_Network.py
@@ -115,3 +115,80 @@ def compute_cost(A2, Y):
     assert (isinstance(cost, float))
 
     return cost
+
+
+def backward_propagation(parameters, cache, X, Y):
+    """
+    Implements the backward propagation, calculating gradients
+
+    Arguments:
+    parameters -- python dictionary containing our parameters
+    cache -- python dictionary containing Z1, A1, Z2, A2
+    X -- input data of shape (n_x, number of examples)
+    Y -- "true" labels vector of shape (n_y, number of examples)
+
+    Returns:
+    grads -- python dictionary containing gradients with respect to different parameters
+    """
+    m = X.shape[1]
+
+
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
+
+    A1 = cache["A1"]
+    A2 = cache["A2"]
+
+    dZ2 = A2 - Y
+    dW2 = 1 / m * np.dot(dZ2, A1.T)
+    db2 = 1 / m * np.sum(dZ2, axis=1, keepdims=True)
+    dZ1 = np.dot(W2.T, dZ2) * A1 * (1 - A1)
+    dW1 = 1 / m * np.dot(dZ1, X.T)
+    db1 = 1 / m * np.sum(dZ1, axis=1, keepdims=True)
+
+    grads = {"dW1": dW1,
+             "db1": db1,
+             "dW2": dW2,
+             "db2": db2}
+
+    return grads
+
+
+def update_parameters(parameters, grads, learning_rate=1.2):
+    """
+    Updates parameters using the gradient descent update rule
+
+    Arguments:
+    parameters -- python dictionary containing parameters
+    grads -- python dictionary containing gradients
+    learning_rate -- learning rate for gradient descent
+
+    Returns:
+    parameters -- python dictionary containing updated parameters
+    """
+    # Retrieve each parameter from the dictionary "parameters".
+
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+
+    # Retrieve each gradient from the dictionary "grads".
+    ### START CODE HERE ### (~ 4 lines of code)
+    dW1 = grads['dW1']
+    db1 = grads['db1']
+    dW2 = grads['dW2']
+    db2 = grads['db2']
+
+    # Update rule for each parameter.
+    W1 = W1 - learning_rate * dW1
+    b1 = b1 - learning_rate * db1
+    W2 = W2 - learning_rate * dW2
+    b2 = b2 - learning_rate * db2
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+
+    return parameters
